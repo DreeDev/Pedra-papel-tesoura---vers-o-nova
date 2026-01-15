f70a121f3eec3fff6a12ccf79ed0fc7b1f0ac3de
DreeDev <miguellm402@gmail.com>
2026-01-15 20:38:56 -0300

    Correções

--- app.py
+++ app.py
@@ -100,4 +100,6 @@ def do_disconnect(reason):
     logger.info('Client disconnected %s %s', request.sid, reason)
 
 if __name__ == '__main__':
-    socketio.run(app)
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
